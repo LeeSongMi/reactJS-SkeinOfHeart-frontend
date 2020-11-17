0b1bd69378fe2756588a6b30fe39d605b8e7ceba
LeeSongMi <33647862+LeeSongMi@users.noreply.github.com>
2020-11-18 02:12:43 +0900

    다이어리 리스트 영역

---
+++ src/js/screens/calendar/ModalForDetailView.js
@@ -0,0 +1,87 @@
+import React, { useEffect } from 'react'
+import { Button } from 'react-bootstrap'
+import Modal from 'react-bootstrap/Modal'
+import 'bootstrap/dist/css/bootstrap.css'
+import '../../../css/detailViewModal.css'
+import { faHeartbeat } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import ReactWordcloud from 'react-wordcloud'
+const ModalForDetailView = ({ diary, diaryModal, setDiaryViewModal, clickDiary }) => {
+    const page = diary[clickDiary === -1 ? 0 : clickDiary]
+    // const title =diary[clickDiary === -1 ? 0 : clickDiary].title
+    const words = [
+        {
+          text: 'told',
+          value: 64,
+        },
+        {
+          text: 'mistake',
+          value: 11,
+        },
+        {
+          text: 'thought',
+          value: 16,
+        },
+        {
+          text: '고양이',
+          value: 200,
+        },
+        {
+            text: '강아지',
+            value: 100,
+          },
+          {
+            text: '안녕',
+            value: 90,
+          },
+          {
+            text: '졸작',
+            value: 18,
+          },
+          {
+            text: '초콜릿',
+            value: 20,
+          },
+          {
+            text: '약',
+            value: 3,
+          },
+        
+      ]
+      const colors = ['#eee7df', '#dbcbbe','#b0988e','#abdeed7','#4d8581']
+    return (
+        <>
+            <Modal id="diaryModal" show={diaryModal} onHide={() => setDiaryViewModal(false)} size="lg" animation={true} aria-labelledby="contained-modal-title-vcenter" centered>
+                <Modal.Header>
+                    <Modal.Title id="contained-modal-title-vcenter">
+                        <div className="viewTitle">{page.title}</div>
+                        <div className="viewDate">작성일: {page.date}</div>
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <div className="diaryArea">
+                        <font className="infoTxt">속마음</font><br />
+                        <font className="contentTxt">{page.content}</font>
+                    </div>
+                    <hr />
+                    <div className="mindArea">
+                        <font className="infoTxt">속마음 분석</font><br />
+                        <font className="infoTxt">워드클라우드</font><br />
+
+                        <ReactWordcloud words={words} color={colors} />
+                    </div>
+                </Modal.Body>
+                <Modal.Footer>
+                    {/* <Button variant="primary" className="moreBtn" onClick={() => MainBtnEventHandler(5)}>
+                        작품 더보기
+                    </Button> */}
+                    <FontAwesomeIcon icon={faHeartbeat} className="mindHeart" color={page.color} />
+                    <Button variant="secondary" className="closeBtn" onClick={() => setDiaryViewModal(false)}>
+                        닫기
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    )
+}
+export default ModalForDetailView
--- src/js/screens/diaryList/DiaryListPresenter.js
+++ src/js/screens/diaryList/DiaryListPresenter.js
@@ -2,17 +2,83 @@ import React, { useState } from 'react'
 import SliderSlick from 'react-slick'
 import ReactDOM from 'react-dom'
 import { getElementError } from '@testing-library/react'
+import ReactWordcloud from 'react-wordcloud'
 
-const DiaryListPresenter = ({ diary, clickDiary, changeDiary, diaryListSlickSetting, cloud, setCloud, diaryModal, setDiaryViewModal, modalHandler }) => {
-    const [isSelected, setSelect] = useState(false)
-
-    console.table(diary[clickDiary === -1 ? 0 : clickDiary])
+const DiaryListPresenter = ({ diary, clickDiary, changeDiary, diaryListSlickSetting, isSelected, cloud, setCloud, diaryModal, setDiaryViewModal, modalHandler }) => {
+    const page = diary[clickDiary === -1 ? 0 : clickDiary]
+    const words = [
+        {
+          text: 'told',
+          value: 64,
+        },
+        {
+          text: 'mistake',
+          value: 11,
+        },
+        {
+          text: 'thought',
+          value: 16,
+        },
+        {
+          text: '고양이',
+          value: 200,
+        },
+        {
+            text: '강아지',
+            value: 100,
+          },
+          {
+            text: '안녕',
+            value: 90,
+          },
+          {
+            text: '졸작',
+            value: 18,
+          },
+          {
+            text: '초콜릿',
+            value: 20,
+          },
+          {
+            text: '약',
+            value: 3,
+          },
+        
+      ]
+      const colors = ['#eee7df', '#dbcbbe','#b0988e','#abdeed7','#4d8581']
     return (
         <div id="diaryList" className="main-div" style={{ height: window.innerHeight }}>
             <div className="content">
                 <div className="title">다이어리 리스트</div>
 
-                <div className="book-content">{isSelected ? <div></div> : <p> 선택된 일기가 없어요 :)</p>}</div>
+                <div className="book-content">
+                    {isSelected ? (
+                        <div id="showDiary">
+                            <div className="diaryLeft">
+                                <div className="viewTitle">{page.title}</div>
+                                <div className="viewDate">작성일: {page.date}</div>
+                                <div className="diaryArea">
+                                    <font className="infoTxt">속마음</font>
+                                    <br />
+                                    <font className="contentTxt">{page.content}</font>
+                                </div>
+                            </div>
+                            {/* <hr /> */}
+                            <div className="diaryRight">
+                                <div className="mindArea">
+                                    <font className="infoTxt">속마음 분석</font>
+                                    <br />
+                                    <font className="infoTxt">워드클라우드</font>
+                                    <br />
+
+                                    <ReactWordcloud words={words} color={colors} className="wordC" />
+                                </div>
+                            </div>
+                        </div>
+                    ) : (
+                        <p className="showDiary"> 선택된 일기가 없어요 :)</p>
+                    )}
+                </div>
                 <SliderSlick {...diaryListSlickSetting}>
                     {diary.map((info, index) => (
                         <>
