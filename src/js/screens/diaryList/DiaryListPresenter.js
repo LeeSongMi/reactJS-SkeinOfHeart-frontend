94ca76bab10cf2765e99333acc0a0b13ea545abb
LeeSongMi <33647862+LeeSongMi@users.noreply.github.com>
2020-11-19 04:05:37 +0900

    슬릭 클릭이 자동변경되는 오류 수정

--- src/js/screens/diaryList/DiaryListPresenter.js
+++ src/js/screens/diaryList/DiaryListPresenter.js
@@ -5,8 +5,9 @@ import { getElementError } from '@testing-library/react'
 import ReactWordcloud from 'react-wordcloud'
 import axios from 'axios'
 import qs from 'qs'
+import { render } from '@testing-library/react'
 
-const DiaryListPresenter = ({ isSelected, clickDiary, changeDiary, diaryListSlickSetting, cloud, setCloud, diaryModal, setDiaryViewModal, modalHandler }) => {
+const DiaryListPresenter = ({ isSelected, setSelect, clickDiary, changeDiary, diaryListSlickSetting, cloud, setCloud, diaryModal, setDiaryViewModal, modalHandler }) => {
     // -- useState & variable
     const [bookList, setBookList] = useState(null)
     const [page, setPage] = useState(null)
@@ -49,21 +50,44 @@ const DiaryListPresenter = ({ isSelected, clickDiary, changeDiary, diaryListSlic
     const scrollToCalender = () => {
         window.scrollTo({ top: window.innerHeight * 3, left: 0, behavior: 'smooth' })
     }
-
+    const clickSlideHandler = (e) => {
+        setSelect(e)
+        render(
+            <style jsx>
+                {`
+                    #diaryList .book .book_cover.a${e} {
+                        border: #abded7 1px solid;
+                        box-shadow: 5px 5px 5px #abded7;
+                    }
+                    #diaryList .book .book_cover.a${isSelected} {
+                        border: none;
+                        box-shadow: 5px 5px 5px #b0988e;
+                    }
+                `}
+            </style>
+        )
+    }
     return (
         <div id="diaryList" className="main-div" style={{ height: window.innerHeight }}>
             <div className="content">
                 <div className="title">다이어리 리스트</div>
 
                 <div className="book-content">
-                    <img className="logo"/>
+                    <img className="logo" />
                     <div className="description">
-                        일기만큼 당신을 들여다 볼 수 있는 건 없을 거에요.<br/>
-                        <b>일기를 클릭</b>해서 당신을 들여다 볼 수 있어요.<br/>
-                        <br/>
-                        혹은 캘린더 화면을 통해 <b>감정 패턴</b>을 살펴볼 수도 있어요.<br/>
-                        감정 패턴을 살펴보고 싶다면,<br/>
-                        <button id="button" onClick={scrollToCalender}> 캘린더로 이동하기 </button>
+                        일기만큼 당신을 들여다 볼 수 있는 건 없을 거에요.
+                        <br />
+                        <b>일기를 클릭</b>해서 당신을 들여다 볼 수 있어요.
+                        <br />
+                        <br />
+                        혹은 캘린더 화면을 통해 <b>감정 패턴</b>을 살펴볼 수도 있어요.
+                        <br />
+                        감정 패턴을 살펴보고 싶다면,
+                        <br />
+                        <button id="button" onClick={scrollToCalender}>
+                            {' '}
+                            캘린더로 이동하기{' '}
+                        </button>
                     </div>
                 </div>
                 <SliderSlick className="diarySlide" {...diaryListSlickSetting}>
@@ -77,11 +101,13 @@ const DiaryListPresenter = ({ isSelected, clickDiary, changeDiary, diaryListSlic
                             </div> */}
                                   <div className="book">
                                       <div
-                                          className="book_cover"
+                                          className={`book_cover a${index}`}
                                           id={index}
                                           onClick={(e) => {
                                               changeDiary(e)
                                               setPage(info)
+                                              clickSlideHandler(e.target.id)
+                                              console.log(e.target.id, 'eee')
                                           }}>
                                           {/* <img src={info.cover} width="80vw" height="auto"></img> */}
                                           <font className="bookContent">{info.content}</font>
