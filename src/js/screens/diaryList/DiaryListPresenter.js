c7498afee16b7ba81953ee5e0c933d739daee893
dev-hjJoo <20163030@changwon.ac.kr>
2020-11-18 23:36:10 +0900

    api 작업중 1차

--- src/js/screens/diaryList/DiaryListPresenter.js
+++ src/js/screens/diaryList/DiaryListPresenter.js
@@ -1,18 +1,49 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import SliderSlick from 'react-slick'
 import ReactDOM from 'react-dom'
 import { getElementError } from '@testing-library/react'
+import axios from 'axios'
+import qs from 'qs'
 
 const DiaryListPresenter = ({ diary, clickDiary, changeDiary, diaryListSlickSetting, cloud, setCloud, diaryModal, setDiaryViewModal, modalHandler }) => {
-    const [isSelected, setSelect] = useState(false)
+    const [isSelected, setSelect] = useState(true)
 
+    useEffect(() => {
+        axios({
+            method: 'POST',
+            url: 'http://127.0.0.1:8000/load_pages',
+            data: qs.stringify({
+                user_no: 1,
+                // content: content,
+            }),
+            headers: {
+                'content-type': 'application/x-www-form-urlencoded',
+            },
+        }).then((response) => {
+            console.log(response)
+        })
+    }, [])
+
+    const text = "안녕 반가워"
     console.table(diary[clickDiary === -1 ? 0 : clickDiary])
     return (
         <div id="diaryList" className="main-div" style={{ height: window.innerHeight }}>
             <div className="content">
                 <div className="title">다이어리 리스트</div>
 
-                <div className="book-content">{isSelected ? <div></div> : <p> 선택된 일기가 없어요 :)</p>}</div>
+                {isSelected ? (
+                    <div className="book-content">
+                        <div className="left-section"> {text}</div>
+                        <div className="right-section">
+                            <div className="right-up-section"></div>
+                            <div className="right-down-section"></div>
+                        </div>
+                    </div>
+                ) : (
+                    <div className="book-content">
+                        <div> 선택된 일기가 없어요 :)</div>
+                    </div>
+                )}
                 <SliderSlick {...diaryListSlickSetting}>
                     {diary.map((info, index) => (
                         <>
